2f759af772a61772809b0748cef7940d56c4f017
gluehbirnenkopf <mail@leongregori.com>
2021-11-20 09:32:26 +0100

    logger added

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import time
 from datetime import datetime
 import os
 
+# logging libs
+import logging
+import sys
+
 import yfinance as yf
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import WritePrecision, SYNCHRONOUS, Point
@@ -11,20 +15,26 @@ from influxdb_client.client.write_api import WritePrecision, SYNCHRONOUS, Point
 class TickerData:
     def __init__(self):
         if "ENABLE_INFLUX" in os.environ:
+            logging.info("Initialize Influx DB...")
             self.influxdbClient = InfluxDBClient(
                 url="http://localhost:8086",
                 token=os.environ.get('INFLUX_TOKEN'),
                 org="my-org"
             )
+            logging.info("Initialize Influx DB successfull")
 
     def run(self):
         while True:
             with open('data/data.json') as data_file:
-                print("loading data file...")
+                logging.info("loading data file...")
                 data = json.load(data_file)
+
                 for action in data:
+                    # initialize ticker for a stock
                     ticker = yf.Ticker(action['signal'])
+                    # get history of that stock
                     history = ticker.history()
+                    # get historical data including last price (Close) and parse only this as last price
                     last_quote = (history.tail(1)['Close'].iloc[0])
                     json_body = [{
                         "measurement": "share_price",
@@ -46,7 +56,7 @@ class TickerData:
                         }
                     }]
 
-                    print("Writing data:"+action['name']+":"+str(last_quote))
+                    logging.info("Writing data:"+action['name']+":"+str(last_quote))
 
                     if "ENABLE_INFLUX" in os.environ:
                         p = Point("stock").tag("name", action['name']).field("price", last_quote).time(datetime.utcnow(),
@@ -60,5 +70,7 @@ class TickerData:
 
 
 if __name__ == "__main__":
-    ticker = TickerData()
-    ticker.run()
+
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    dataSource = TickerData()
+    dataSource.run()
