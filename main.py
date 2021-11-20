3bba1333ce558369b108018463e7216ba0ace068
gluehbirnenkopf <mail@leongregori.com>
2021-11-20 09:56:55 +0100

    externalise influx client

--- main.py
+++ main.py
@@ -14,14 +14,23 @@ from influxdb_client.client.write_api import WritePrecision, SYNCHRONOUS, Point
 
 class TickerData:
     def __init__(self):
-        if "ENABLE_INFLUX" in os.environ:
+        logging.info("ENABLE_INFLUX: "+str(os.getenv("ENABLE_INFLUX")))
+        if os.getenv("ENABLE_INFLUX"):
             logging.info("Initialize Influx DB...")
             self.influxdbClient = InfluxDBClient(
                 url="http://localhost:8086",
-                token=os.environ.get('INFLUX_TOKEN'),
-                org="my-org"
+                token=os.getenv('INFLUX_TOKEN'),
+                org="ccc"
             )
             logging.info("Initialize Influx DB successfull")
+
+    def writeStockPriceInflux(self, name, price):
+        p = Point("stock").tag("name", name).field("price", price).time(datetime.utcnow(),WritePrecision.MS)
+        write_api = self.influxdbClient.write_api(write_options=SYNCHRONOUS)
+
+        # write using point structure
+        logging.info("db write")
+        write_api.write(bucket="ccc", record=p)
 
     def run(self):
         while True:
@@ -59,12 +68,7 @@ class TickerData:
                     logging.info("Writing data:"+action['name']+":"+str(last_quote))
 
                     if "ENABLE_INFLUX" in os.environ:
-                        p = Point("stock").tag("name", action['name']).field("price", last_quote).time(datetime.utcnow(),
-                                                                                                      WritePrecision.MS)
-                        write_api = self.influxdbClient.write_api(write_options=SYNCHRONOUS)
-
-                        # write using point structure
-                        write_api.write(bucket="ccc", record=p)
+                        self.writeStockPriceInflux(action['name'], last_quote)
 
                     time.sleep(20)
 
